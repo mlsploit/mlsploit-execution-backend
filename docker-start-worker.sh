d9899a7949107c13c0bb5a6f05f863b2e0f75095
Nilaksh Das <nilakshdas@gatech.edu>
2019-11-18 00:07:49 -0500

    Change project name for docker services for worker

--- docker-start-worker.sh
+++ docker-start-worker.sh
@@ -4,12 +4,16 @@ set -a && . .env && set +a
 
 MLSPLOIT_DOCKER_HOST_BASE_DIR="$(pwd)";
 
+DOCKER_PROJECT_NAME=mlsploit-execution-backend-worker
+
 docker-compose \
+    -p ${DOCKER_PROJECT_NAME} \
     -f services.docker-compose.execution.yml \
     build \
     --build-arg STAGE=worker \
     --build-arg BASE_DIR="${MLSPLOIT_DOCKER_HOST_BASE_DIR}"
 
 docker-compose \
+    -p ${DOCKER_PROJECT_NAME} \
     -f services.docker-compose.execution.yml \
     up
